ba77c3d019a0dbbde8038c70d5aa2aa75fab7a4b
Jakub Opatowski <jakub.opatowski@gmial.com>
2011-04-06 19:24:09 +0200

    Nowy projekt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rotmistrz2::Application.config.secret_token = 'd5db55b9f386280900b91eff5929b750b8386a36ea05be90f886d8fff9a0434210aa33560c0082337d324c501473d573fec453429c3de2713b53973ca0347442'
